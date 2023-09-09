30826775c635cdb0ce77ee1c5aa4e8891c9ac1c9
SanjeevThalod <thalodsanjeev@gmail.com>
2023-09-09 20:12:48 +0530

    railwaycommit

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const app = express();
 app.use(cors());
 
 
+//Connecting to DB
+mongodb();
+
 // Initiliazing food DB model
 const foodData = mongoose.model('foodData',foodSchema,'foodItems');
 const catData = mongoose.model('catData',catSchema,'foodcatagory');
@@ -43,12 +46,6 @@ app.post('/food',async (req,res)=>{
     } catch (error) {
         res.status(400).json(error.message);
     }
-})
-
-mongodb().then(() => {
-    app.listen(5001, () => {
-        console.log("listening for requests");
-    })
 })
 
 app.listen(5000,console.log('Listening on 5000'));
